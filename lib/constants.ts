497c13e0bb4378c294911a7892f9eaec605ad699
ebrarhosgul <ebrarmuhammedhosgul@gmail.com>
2025-12-09 04:28:40 +0300

    refactor: centralization of configuration constants

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+export const EARTH_RADIUS_KM = 6371;
+export const MS_TO_FPM = 196.85;
+export const REFRESH_RATE_MS = 4000;
+export const HISTORY_POINTS_COUNT = 30;
+
+export const NEAR_AIRPORT_DISTANCE_KM = 150;
+export const LOW_ALTITUDE_THRESHOLD_FT = 4000;
